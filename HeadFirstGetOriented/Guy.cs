14f1cfead8df43207f1d1dc401b23fc0c405fe2b
innight <joaosemedo16@hotmail.com>
2021-07-28 08:10:40 +0100

    Exercicio objects:get oriented

---
+++ HeadFirstGetOriented/Guy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstGetOriented
+{
+    public class Guy
+    {
+        private string Name;
+        private int Age;
+        private int Cash;
+
+        public Guy(string name,int age, int cash)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Cash = cash;
+
+        }
+
+       public Guy() { }
+
+        public int GiveCash(int amount)
+        {
+            if(amount <= Cash && amount > 0)
+            {
+                Cash -= amount;
+                return amount;
+            }
+            else
+            {
+
+                Console.WriteLine("I dont have enough cash!" + amount, Name + "says...");
+                return 0;
+            }
+        }
+
+        public int ReceiveCash(int amount)
+        {
+            if (amount > 0)
+            {
+                Cash += amount;
+                return amount;
+            }
+            else
+            {
+                Console.WriteLine(amount + " isn´t an amount i ll take", Name + "says...");
+                return 0;
+            }
+        }
+
+        public int ShowCash()
+        {
+            return Cash;
+        }
+
+    }
+}
--- HeadFirstGetOriented/Program.cs
+++ HeadFirstGetOriented/Program.cs
@@ -4,9 +4,53 @@ namespace HeadFirstGetOriented
 {
     class Program
     {
+
+
+        private static bool MainMenu(Guy Bob, Guy Joe)
+        {
+          
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Bob GIVE 10 dollars to Joe");
+            Console.WriteLine("2) Joe Receive 5 dollares from bob");
+            Console.WriteLine("3) Show Cash");
+            Console.WriteLine("4) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Bob.ReceiveCash(Joe.GiveCash(10));
+                    return true;
+                case "2":
+                    Joe.ReceiveCash(Bob.GiveCash(5));
+                    return true;
+                case "3":
+                    Console.WriteLine(Bob.ShowCash()); 
+                    return true;
+                case "4":
+                    Console.WriteLine("EXIT");
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            Guy Bob = new Guy("Bob",10,50)   ;
+            Guy Joe = new Guy("Joe", 10, 50);
+            int bank = 100;
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu(Bob,Joe);
+            }
+
             Console.WriteLine("Hello World!");
+          
         }
     }
 }
